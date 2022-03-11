25a03175a813a5326aa5ea7199ce3a538f000cee
Racert435 <hidas435@gmail.com>
2022-03-11 09:17:46 -0600

    first commit2

--- index.js
+++ index.js
@@ -83,128 +83,6 @@ function myFunction() {
   document.getElementById("Distribuir6").innerHTML = text7;
 }
 
-function capturar(){
-  var c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24;
-   
-  c1 = document.getElementById("num3").value;
-  c2 = document.getElementById("num4").value;
-  c3 = document.getElementById("num5").value;
-  c4 = document.getElementById("num6").value;
-  c5 = document.getElementById("num7").value;
-  c6 = document.getElementById("num8").value;
-  c7 = document.getElementById("num9").value;
-  c8 = document.getElementById("num10").value;
-  c9 = document.getElementById("num11").value;
-  c10 = document.getElementById("num12").value;
-  c11 = document.getElementById("num13").value;
-  c12 = document.getElementById("num14").value;
-  c13 = document.getElementById("num15").value;
-  c14 = document.getElementById("num16").value;
-  c15 = document.getElementById("num17").value;
-  c16= document.getElementById("num18").value;
-  c17 = document.getElementById("num19").value;
-  c18 = document.getElementById("num20").value;
-  c19 = document.getElementById("num21").value;
-  c20 = document.getElementById("num22").value;
-  c21 = document.getElementById("num23").value;
-  c22 = document.getElementById("num24").value;
-  c23 = document.getElementById("num25").value;
-  c24 = document.getElementById("num26").value;
-
-  if(isNaN(c1) || isNaN(c2)){
-    text = "Es necesarios introducir dos números válidos";
-  }else{
-
-  co1=c1;
-  co2=c2;
-  co3=c3;
-  co4=c4;
-  co5=c5;
-  co6=c6;
-  co7=c7;
-  co8=c8;
-  co9=c9;
-  co10=c10;
-  co11=c11;
-  co12=c12;
-  co13=c13;
-  co14=c14;
-  co15=c15;
-  co16=c16;
-  co17=c17;
-  co18=c18;
-  co19=c19;
-  co20=c20;
-  co21=c21;
-  co22=c22;
-  co23=c23;
-  co24=c24;
-
-  text1=co1;
-  text2=co2;
-  text3=co3;
-  text4=co4;
-  text5=co5;
-  text6=co6;
-  text7=co7;
-  text8=co8;
-  text9=co9;
-  text10=co10;
-  text11=co11;
-  text12=co12;
-  text13=co13;
-  text14=co14;
-  text15=co15;
-  text16=co16;
-  text17=co17;
-  text18=co18;
-  text19=co19;
-  text20=co20;
-  text21=co21;
-  text22=co22;
-  text23=co23;
-  text24=co24;
-
-
-
-
-
-
-  
-     
-  }
-
-
-
-
-
-  document.getElementById("comu1").innerHTML = text1;
-  document.getElementById("comu2").innerHTML = text2;
-  document.getElementById("comu3").innerHTML = text3;
-  document.getElementById("comu4").innerHTML = text4;
-  document.getElementById("comu5").innerHTML = text5;
-  document.getElementById("comu6").innerHTML = text6;
-  document.getElementById("comu7").innerHTML = text7;
-  document.getElementById("comu8").innerHTML = text8;
-  document.getElementById("comu9").innerHTML = text9;
-  document.getElementById("comu10").innerHTML = text10;
-  document.getElementById("comu11").innerHTML = text11;
-  document.getElementById("comu12").innerHTML = text12;
-  document.getElementById("comu13").innerHTML = text13;
-  document.getElementById("comu14").innerHTML = text14;
-  document.getElementById("comu15").innerHTML = text15;
-  document.getElementById("comu16").innerHTML = text16;
-  document.getElementById("comu17").innerHTML = text17;
-  document.getElementById("comu18").innerHTML = text18;
-  document.getElementById("comu19").innerHTML = text19;
-  document.getElementById("comu20").innerHTML = text20;
-  document.getElementById("comu21").innerHTML = text21;
-  document.getElementById("comu22").innerHTML = text22;
-  document.getElementById("comu23").innerHTML = text23;
-  document.getElementById("comu24").innerHTML = text24;
-
-
-}
 
 
 function repartir(){
@@ -249,12 +127,16 @@ function repartir(){
     text5 = variable3;
     text6 = Comprobacion;
     text7 = Comprobacion2;
+    
+    
+    
 
 
     for(i=1;i<=numhijo1;i++){
-      caja=numhijo
-      document.getElementById("comu1").innerHTML = caja;
-      document.getElementById("comu2").innerHTML = caja;
+      
+      document.getElementById("comu1").innerHTML = numhijo;
+    
+    
      
     
     } 
@@ -263,18 +145,19 @@ function repartir(){
     var m=y;
 
     for(var j=w; j<=m ; j++){
-      caja=numpadre
-
-      document.getElementById("comu1").innerHTML = caja;
-      document.getElementById("comu2").innerHTML = caja;
+      
+      document.getElementById("comu2").innerHTML = numpadre;
      
     }
+    
+    
+
 
   
   
 }
 
-    
+
 
 
  
